3e8eaf004d60e028867b3232615d798d3d79aeb2
peculiaruc <peculiarumeh02@gmail.com>
2020-12-29 14:44:46 +0100

    Calculator App

---
+++ app/src/main/java/com/scainitiative/kotlinscainitiativeproject/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.scainitiative.kotlinscainitiativeproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }, 6000)
+    }
+}
